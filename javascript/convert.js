266e1135d64165babe8f8e8622994dd5c541b998
Ben Driggs <89993894+StarLicker@users.noreply.github.com>
2023-10-30 12:32:27 -0600

    Started JavaScript for convert page

---
+++ javascript/convert.js
@@ -0,0 +1,14 @@
+//TODO: keep track of if user is logged in or not (aka. for scoreboard/history display guest#123 if user hasn't logged in)
+//TODO: store measurement type and object pair
+//TODO: Diplay placeholder conversion result
+
+function convert() {
+    const measurementOptions = document.querySelectorAll("varMeasurement");
+    const measurementType = null;
+
+    for (let measurement in measurementOptions) {
+        if (measurement.checked) {
+            
+        }
+    }
+}
