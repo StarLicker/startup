1c493f5deec9888c72cf14b105e78230d92222a0
Ben Driggs <89993894+StarLicker@users.noreply.github.com>
2023-11-08 23:40:41 -0700

    Adjusted directories to fix node debugging

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 // For node implementation
 
-import express from 'express';
+const express = require('express');
 const app = express();
 
-const port = process.arbv.length > 2 ? process.argv[2] : 4000;
+//const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const port = 4000;
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
 
-// JSON body parsing using built-in middleware
+
+//JSON body parsing using built-in middleware
 app.use(express.json());
 
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
 
 // Router for service endpoints
-var apiRouter = express.Router();
-app.use('/api',apiRouter);
+// var apiRouter = express.Router();
+// app.use('/api',apiRouter);
 
 // Store conversion
 // apiRouter.post('/convert')
