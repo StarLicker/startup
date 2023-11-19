9a41f7a77a4dae6ecd3217cd9ce0b886177e518e
Ben Driggs <bendriggs99@gmail.com>
2023-11-18 19:50:21 -0700

    More index.js adjustments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-var dotenv = require('dotenv').config("C:\\Users\\benny\\OneDrive\\Desktop\\CS260\\startup\\.env");
+var dotenv = require('dotenv').config({path: __dirname + '/.env'});
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // Setting up openai using apikey
