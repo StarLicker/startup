c2a37b501c1f240b040271f6ded4b795efaed701
Ben Driggs <89993894+StarLicker@users.noreply.github.com>
2023-12-14 15:14:54 -0700

    Moved original html over to src

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className="body">
+      <div className="spinner"><img src="/assets/conversion.png" alt="conversion image" width="150"></img></div>
+      <header>
+        <h1>
+            Gotta Convert Them All
+        </h1>
+        <hr />
+      </header>
+      <main>App components go here</main>
+      <footer>
+        <nav>
+            <li><a className="github" href="https://github.com/StarLicker/startup/tree/main">GitHub Repo</a></li>
+        </nav>
+    </footer>
+    </div>
+  );
 }
